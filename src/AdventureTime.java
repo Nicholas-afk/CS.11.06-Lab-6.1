580725eef222d9c363166d2e87d082d62840afa0
Nicholas-afk <64998158+Nicholas-afk@users.noreply.github.com>
2024-11-10 01:11:24 +0800

    Update AdventureTime.java

--- src/AdventureTime.java
+++ src/AdventureTime.java
@@ -9,12 +9,20 @@ public class AdventureTime {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
+        int challengeOneAnswer = challengeOne("inputOneTwo.txt");
+        int challengeTwoAnswer = challengeTwo("inputOneTwo.txt");
+        int challengeThreeAnswer = challengeThree("inputThreeFour.txt");
+        int challengeFourAnswer = challengeFour("inputThreeFour.txt");
 
+        System.out.println(challengeOneAnswer);
+        System.out.println(challengeTwoAnswer);
+        System.out.println(challengeThreeAnswer);
+        System.out.println(challengeFourAnswer);
 
+        writeFileAllAnswers("AdventureTime.txt", challengeOneAnswer, challengeTwoAnswer, challengeThreeAnswer, challengeFourAnswer);
     }
 
-    /** TODO 1
-     *
+    /**
      * Challenge 1
      *
      * @param fileName
@@ -22,11 +30,18 @@ public class AdventureTime {
      * @throws IOException
      */
     public static int challengeOne(String fileName) throws IOException {
-        return 0;
+        int[] data = readFile(fileName);
+        int increaseCount = 0;
+
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] > data[i - 1]) {
+                increaseCount++;
+            }
+        }
+        return increaseCount;
     }
 
-    /** TODO 2
-     *
+    /**
      * Challenge 2
      *
      * @param fileName
@@ -34,11 +49,21 @@ public class AdventureTime {
      * @throws FileNotFoundException
      */
     public static int challengeTwo(String fileName) throws FileNotFoundException {
-        return 0;
+        int[] data = readFile(fileName);
+        int increaseCount = 0;
+
+        for (int i = 3; i < data.length; i++) {
+            int previousSum = data[i - 3] + data[i - 2] + data[i - 1];
+            int currentSum = data[i - 2] + data[i - 1] + data[i];
+
+            if (currentSum > previousSum) {
+                increaseCount++;
+            }
+        }
+        return increaseCount;
     }
 
-    /** TODO 3
-     *
+    /**
      * Challenge 3
      *
      * @param fileName
@@ -46,11 +71,33 @@ public class AdventureTime {
      * @throws FileNotFoundException
      */
     public static int challengeThree(String fileName) throws FileNotFoundException {
-        return 0;
+        int horizontalPosition = 0;
+        int depth = 0;
+
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(" ");
+                String command = parts[0];
+                int value = Integer.parseInt(parts[1]);
+
+                switch (command) {
+                    case "forward":
+                        horizontalPosition += value;
+                        break;
+                    case "down":
+                        depth += value;
+                        break;
+                    case "up":
+                        depth -= value;
+                        break;
+                }
+            }
+        }
+        return horizontalPosition * depth;
     }
 
-    /** TODO 4
-     *
+    /**
      * Challenge 4
      *
      * @param filename
@@ -58,7 +105,32 @@ public class AdventureTime {
      * @throws FileNotFoundException
      */
     public static int challengeFour(String filename) throws FileNotFoundException {
-        return 0;
+        int horizontalPosition = 0;
+        int depth = 0;
+        int aim = 0;
+
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(" ");
+                String command = parts[0];
+                int value = Integer.parseInt(parts[1]);
+
+                switch (command) {
+                    case "forward":
+                        horizontalPosition += value;
+                        depth += aim * value;
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                    case "up":
+                        aim -= value;
+                        break;
+                }
+            }
+        }
+        return horizontalPosition * depth;
     }
 
     /** This method will write the values passed as challengeOne, challengeTwo, challengeThree, and challengeFour to a text file.
@@ -104,5 +176,4 @@ public class AdventureTime {
         scanner.close();
         return lineCount;
     }
-
 }
